52588cfb3dd88ebbfa2a8d05dd9c3e5b94d7275f
Lucas <wehrlucasjh@gmail.com>
2026-03-12 17:54:54 +0100

    Ajout trie technologie

--- Elysea/Assets/script/Ruin/SelectTechnologyPanel.cs
+++ Elysea/Assets/script/Ruin/SelectTechnologyPanel.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+using Unity.VisualScripting;
+using System;
 
 public class SelectTechnologyPanel : MonoBehaviour
 {
@@ -10,4 +13,47 @@ public class SelectTechnologyPanel : MonoBehaviour
     public Button Button2;
     public Button Button3;
     public TextMeshProUGUI TitleText;
+    public TextAsset TechFile;
+    private List<string[]> AllTechnologies = new List<string[]>();
+    private List<string[]> AllowTechnologies = new List<string[]>();
+    private List<string[]> CurrentTechnologies = new List<string[]>();  
+    private bool isAllowUr = false;
+
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+
+        // Load technology data from the text file
+        foreach(string line in TechFile.text.Split('\n'))
+        {
+            string[] data = line.Split(',');
+
+            if (int.TryParse(data[0], out int result) && data[15] == "")
+            {
+                AllTechnologies.Add(data);
+            }
+        }
+
+        foreach (string[] tech in CurrentTechnologies) if(tech[2] == "Lab. Nucléaire") isAllowUr = true;
+
+
+        // Technologies that are currently available to the player
+        foreach (string[] tech in AllTechnologies)
+        {
+            if (tech[12]=="" || (isAllowUr && tech[12] == "Lab. Nucléaire"))
+            {
+                AllowTechnologies.Add(tech);
+            }
+        }
+
+        System.Random rand = new System.Random();
+        var randomValues = AllowTechnologies.OrderBy(x => rand.Next()).Take(3).ToList();
+
+        Button1.GetComponentInChildren<TextMeshProUGUI>().text = randomValues[0][2];
+        Button2.GetComponentInChildren<TextMeshProUGUI>().text = randomValues[1][2];
+        Button3.GetComponentInChildren<TextMeshProUGUI>().text = randomValues[2][2];
+
+    
+    }
 }
+
