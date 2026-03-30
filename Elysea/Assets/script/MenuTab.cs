4584df4f7b3f8b3dd7eaff6af697a161bec47470
julienBernard3 <julienbernardjb88@gmail.com>
2026-03-30 11:25:04 +0200

    Ajout de la vérification de l'objet menu dans la méthode Start de MenuTab

--- Elysea/Assets/script/MenuTab.cs
+++ Elysea/Assets/script/MenuTab.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class MenuTab : MonoBehaviour
 {
+    // [HideInInspector]
     public GameObject menu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (menu == null)
+        {
+            GameObject canvas = GameObject.Find("Canvas");
+            if (canvas != null){
+                menu = canvas.transform.Find("MenuTab").gameObject;
+                if (menu == null)
+                    Debug.LogWarning("MenuTab: MenuTab non trouvé");
+            }
+            else
+                Debug.LogWarning("MenuTab: Canvas non trouvé");
+        }
     }
 
     // Update is called once per frame
