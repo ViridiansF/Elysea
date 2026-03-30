3ec7d174981b28e5f695cf3d26ebe563ed846efa
julienBernard3 <julienbernardjb88@gmail.com>
2026-03-30 11:41:19 +0200

    Modification de l'annotation HideInInspector pour le champ menu dans la classe MenuTab

--- Elysea/Assets/script/MenuTab.cs
+++ Elysea/Assets/script/MenuTab.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MenuTab : MonoBehaviour
 {
-    // [HideInInspector]
+    [HideInInspector]
     public GameObject menu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
