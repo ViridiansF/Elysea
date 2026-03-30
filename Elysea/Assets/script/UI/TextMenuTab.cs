7649ddb53958821d968f25c19bb9970b88f9f33f
julienBernard3 <julienbernardjb88@gmail.com>
2026-03-30 11:32:07 +0200

    Ajout de la recherche de l'objet TechPanel dans la méthode Start de TextMenuTab

--- Elysea/Assets/script/UI/TextMenuTab.cs
+++ Elysea/Assets/script/UI/TextMenuTab.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class TextMenuTab : MonoBehaviour
 {
+    [HideInInspector]
     public SelectTechnologyPanel TechPanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (TechPanel == null){
+            GameObject canvas = GameObject.Find("Canvas");
+            if (canvas != null){
+                TechPanel = canvas.GetComponentInChildren<SelectTechnologyPanel>(includeInactive: true);
+                if (TechPanel == null)
+                    Debug.LogWarning("ruinCollision: TechPanel non trouvé");
+            }
+            else
+                Debug.LogWarning("ruinCollision: Canvas non trouvé");
+        }
     }
 
     // Update is called once per frame
